806380aea5a5f611a29902e2d7c3de40243bf862
vince <vincent.harpin@orange.fr>
2023-02-17 15:20:47 +0100

    exercice 200

--- delete.php
+++ delete.php
@@ -1,1 +1,26 @@
 <?php
+    $server = 'localhost';
+    $user = 'root';
+    $pwd = '';
+    $db = 'liste_randonnée';
+
+    try {
+        $connect = new PDO("mysql:host=$server;dbname=$db;charset=utf8", $user, $pwd);
+
+        $id = $_POST['valeur'];
+
+        $efface = "DELETE FROM hiking WHERE id = $id";
+
+        if($connect->exec($efface) !== false){
+            echo "Une randonnée a bien été supprimé";
+        }
+    }
+    catch (PDOException $exception) {
+        echo "Erreur de connexion: " . $exception->getMessage();
+    }
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'read.php';
+header("Location: http://$host$uri/$extra");
+exit;
